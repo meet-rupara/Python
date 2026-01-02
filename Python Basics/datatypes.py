4d9a5c59424153174ff5d74599ac200455159332
Meet Rupara <meet.rupara@gmail.com>
2026-01-02 23:40:32 +0530

    datatypes

---
+++ Python Basics/datatypes.py
@@ -0,0 +1,15 @@
+# DataTypes In Python 
+
+
+a = 5    #int 
+b =1.234  #float
+c =" meet "  #string
+d  = True  #boolean 
+e = None  #none
+
+print("thr types are : ",a,b,c,d,e)
+print(type(a))
+print(type(b))
+print(type(c))
+print(type(d))
+print(type(e))
